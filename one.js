7d172496849eed6a6a692e621c658b3ebf4b0850
rajat <rajatgayakwad15@gmail.com>
2024-08-16 11:38:37 +0530

    ooooo

---
+++ one.js
@@ -0,0 +1,16 @@
+let id=1
+let name="rajat"
+let emailid="rajatgayakwad@gmail.com"
+var password;
+name="omkar"
+emailid="omkar@gmail.com"
+console.table([id, name, emailid, password])
+
+let isloggedin = false
+let state
+
+console.log(typeof "Shubham")
+let age=16;
+console.log(typeof age)
+let dis=null
+console.log(dis)
